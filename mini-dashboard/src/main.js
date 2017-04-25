f0c15037535e1deec3062148a3f0ceff85fc6bbd
Hector Sevilla <h2d_07@hotmail.com>
2017-04-25 17:40:13 -0600

    Added vuikit

--- mini-dashboard/src/main.js
+++ mini-dashboard/src/main.js
@@ -1,7 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import config from './config.js'
+import Vuikit from 'vuikit'
+
+Vue.use(Vuikit)
+
+
 
 new Vue({
   el: '#app',
+  Vuikit,
   render: h => h(App)
 })
